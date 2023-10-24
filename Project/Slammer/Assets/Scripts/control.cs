fcc889cac686eb36647a17907f2e910c5bf62c27
Araneae <penguin13790@gmail.com>
2023-10-24 17:15:24 -0400

    Project Start

---
+++ Project/Slammer/Assets/Scripts/control.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float mod;
+
+    void FixedUpdate() {
+        Vector2 v = new Vector2();
+        if (Input.GetKey("w")) v.y += 1;
+        if (Input.GetKey("a")) v.x -= 1;
+        if (Input.GetKey("s")) v.y -= 1;
+        if (Input.GetKey("d")) v.x += 1;
+
+        if (!(v.x == 0 || v.y == 0)) {
+            v *= Mathf.Sqrt(2) / 2;
+        }
+
+        v *= mod;
+        rb.velocity = v;
+    }
+}
