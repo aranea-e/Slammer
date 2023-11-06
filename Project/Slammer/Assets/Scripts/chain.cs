6eab41e24715af09684f090db92bca3c570244c5
Araneae <penguin13790@gmail.com>
2023-11-06 12:02:26 -0500

    Final
    +Built and published to Itch

--- Project/Slammer/Assets/Scripts/chain.cs
+++ Project/Slammer/Assets/Scripts/chain.cs
@@ -6,19 +6,18 @@ public class chain : MonoBehaviour {
     public GameObject prefab;
     public List<GameObject> links;
     public float linksLength = Mathf.Sqrt(2) * 3 / 16;
-    public control player;
+    public Transform player;
 
     void Update() {
-        Vector3 playerPos = player.transform.position - new Vector3(0.25f, 0.25f, 0);
         for (int i = 0; i < links.Count; i++) {
             Destroy(links[i]);
         }
         links.Clear();
         
         if (gameObject.GetComponent<hook>().state == "retracted") { return; }
-        float distance = Vector3.Distance(playerPos, transform.position);
+        float distance = Vector3.Distance(player.position, transform.position);
         int numLinks = (int) Mathf.Ceil(distance / linksLength);
-        Vector2 direction = playerPos - transform.position;
+        Vector2 direction = player.position - transform.position;
         Vector3 rotation = new Vector3(0, 0, (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) + 135f);
         Quaternion q = new Quaternion();
         q.eulerAngles = rotation;
@@ -26,7 +25,7 @@ public class chain : MonoBehaviour {
 
         for (int i = 0; i < numLinks; i++) {
             float p = i / (float) numLinks;
-            links.Add(Instantiate(prefab, (playerPos * (1 - p)) + (transform.position * p), q));
+            links.Add(Instantiate(prefab, (player.position * (1 - p)) + (transform.position * p), q));
             links[i].transform.parent = this.transform;
         }
     }
